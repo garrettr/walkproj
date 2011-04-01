234b15fff26c0ed2d1b454d55abbdb92da99e557
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-01 02:31:52 -0400

    Added home view, shows 5 most recently signed-up participants. Will take a bit more work to show participants with the most overall amount pledged. Also renamed original template to be consistent with view/urls.

--- walkathon/views.py
+++ walkathon/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from walkathon.models import Participant, Sponsor
+
+def home(request):
+    '''
+    Display top 10 participants, and just registered or something.
+    Have links for donating and registering to walk.
+    '''
+    recent_participants = Participant.objects.all().order_by('created_on')[:5]
+    return render_to_response('walkathon/home.html', 
+            {'recent_participants': recent_participants})
