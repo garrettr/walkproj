11bfd440210cae2c7ca15d82883c8494b9a640b9
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-01 02:13:30 -0400

    Wrote basic models. Onto writing forms for them...

--- walkathon/models.py
+++ walkathon/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+# TODO  More contact info (address, phone #)?
+
 # Create your models here.
+class Participant(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    statement = models.TextField()
+    # picture
+
+    active = models.BooleanField()
+    created_on = models.DateTimeField()
+
+class Sponsor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    statement = models.TextField()
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    is_sponsoring = models.ForeignKey(Participant)
+
+    active = models.BooleanField()
+    created_on = models.DateTimeField()
