5e05ca051df9de7e9a5274e10e309c81b924d4e3
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-01 02:44:46 -0400

    Added walkathon to INSTALLED_APPS

--- settings.py
+++ settings.py
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'messages',
+    'walkathon',
 )
 
 # A sample logging configuration. The only tangible logging
