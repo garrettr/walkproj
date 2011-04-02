ef75db5559fba7fe9aa19427975080c38637f74a
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-01 22:17:02 -0400

    Sexified the admin interface a bit.

--- walkathon/admin.py
+++ walkathon/admin.py
@@ -3,5 +3,11 @@
 from django.contrib import admin
 from walkathon.models import Participant, Sponsor
 
-admin.site.register(Participant)
-admin.site.register(Sponsor)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'created_on')
+
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'created_on')
+
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Sponsor, SponsorAdmin)
