db14e01cb5a840a825d7128bc6ea149f6dad5003
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-02 16:19:32 -0400

    Fixed "recent" lists to be ordered correctly.

--- walkathon/views.py
+++ walkathon/views.py
@@ -7,8 +7,8 @@ def home(request):
     Display top 10 participants, and just registered or something.
     Have links for donating and registering to walk.
     '''
-    recent_participants = Participant.objects.all().order_by('created_on')[:5]
-    recent_sponsors = Sponsor.objects.all().order_by('created_on')[:5]
+    recent_participants = Participant.objects.all().order_by('-created_on')[:5]
+    recent_sponsors = Sponsor.objects.all().order_by('-created_on')[:5]
     return render_to_response('walkathon/home.html', 
             {'recent_participants': recent_participants,
                 'recent_sponsors': recent_sponsors})
